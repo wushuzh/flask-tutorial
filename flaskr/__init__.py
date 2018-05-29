c848ad0b08937674a269041ecbf07f9c7f3f1dd5
wushuzh <zhangjiaqiang@gmail.com>
2018-05-29 11:10:40 +0800

    add blueprint auth and reg to app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -31,4 +31,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    # apply the blueprints to the app
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
