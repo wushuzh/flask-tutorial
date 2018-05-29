b41403406d541f4c2a130fdef9330303b3ab3dd4
wushuzh <zhangjiaqiang@gmail.com>
2018-05-29 11:22:30 +0800

    add view func for user login

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,7 +1,7 @@
 from flask import (
-    Blueprint, request, redirect, url_for, flash, render_template
+    Blueprint, request, redirect, url_for, flash, render_template, session
 )
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -39,3 +39,29 @@ def register():
         flash(error)
 
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
