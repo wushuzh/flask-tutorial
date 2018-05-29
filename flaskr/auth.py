5c4354e0df7451427e4dc19caaaa8ab60eb3698e
wushuzh <zhangjiaqiang@gmail.com>
2018-05-29 11:14:07 +0800

    add view func for user register

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,3 +1,41 @@
-from flask import Blueprint
+from flask import (
+    Blueprint, request, redirect, url_for, flash, render_template
+)
+from werkzeug.security import generate_password_hash
+from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    """Register a new user.Blueprint
+
+    Validates that the username is not already taken.
+    Hashes the password for security
+    """
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username =?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
