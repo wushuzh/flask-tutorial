d8e66fe3f163fbad96eeeba29931e7e2ed29f487
wushuzh <zhangjiaqiang@gmail.com>
2018-06-12 16:32:45 +0800

    Add user register test cases

---
+++ tests/test_auth.py
@@ -0,0 +1,16 @@
+import pytest
+
+from flaskr.db import get_db
+
+
+def test_register(client, app):
+    assert client.get('/auth/register').status_code == 200
+    response = client.post(
+        '/auth/register', data={'username': 'a', 'password': 'a'}
+    )
+    assert 'http://localhost/auth/login' == response.headers['Location']
+
+    with app.app_context():
+        assert get_db().execute(
+            "SELECT * FROM user WHERE username = 'a'",
+        ).fetchone() is not None
