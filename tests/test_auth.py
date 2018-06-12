cfdf22027f5693332f4cfcc49908203d8c65dda8
wushuzh <zhangjiaqiang@gmail.com>
2018-06-12 16:48:53 +0800

    Add user register robost test cases

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -14,3 +14,16 @@ def test_register(client, app):
         assert get_db().execute(
             "SELECT * FROM user WHERE username = 'a'",
         ).fetchone() is not None
+
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('', '', b'Username is required.'),
+    ('a', '', b'Password is required.'),
+    ('test', 'test', b'already registered.'),
+))
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
