223562dc12179a4adcaf27c1e4f78c5762675b64
wushuzh <zhangjiaqiang@gmail.com>
2018-06-13 08:43:02 +0800

    Add user name and pwd validation test cases

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -38,3 +38,12 @@ def test_login(client, auth):
         client.get('/')
         assert session['user_id'] == 1
         assert g.user['username'] == 'test'
+
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('a', 'test', b'Incorrect username.'),
+    ('test', 'a', b'Incorrect password.'),
+))
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data
